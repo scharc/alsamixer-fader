dfabef0ba6488b06b5fbfbcad0df6005150224ea
Marc Schütze <scharc@gmail.com>
2020-10-08 16:37:37 +0200

    Update fade.sh
    Removed all the debug echo's

--- fade.sh
+++ fade.sh
@@ -2,30 +2,22 @@
 VOLUMECHANNEL="Digital"
 VOLUMEALSA=$(amixer -M get $VOLUMECHANNEL | grep -m 1 -o "...%" |head -1)
 VOLUMEALSA=${VOLUMEALSA//[^[:digit:].]/}
-echo "$VOLUMEALSA"
 FADETO=$1
 SPEED=$2
 VOLUMESTEP=1
 FADEVOLUME=$VOLUMEALSA
 if [[ $VOLUMEALSA -gt $FADETO ]]; then
-    echo "Fade out"
-    # Fading out and stop music
+    # Fading Out
     until [[ $FADEVOLUME -le $FADETO || $FADEVOLUME -le 0 ]]; do
         FADEVOLUME=$(($FADEVOLUME-$VOLUMESTEP))
-        echo "Fadevolume $FADEVOLUME"
         amixer -q -M set "$VOLUMECHANNEL" "${FADEVOLUME}%"
         sleep $SPEED
     done
 else 
-    echo "Fade in"
+    # Fading in
     until [[ $FADEVOLUME -ge $FADETO || $FADEVOLUME -ge 100 ]]; do
         FADEVOLUME=$(($FADEVOLUME+$VOLUMESTEP))
-        echo "Fadevolume $FADEVOLUME"
         amixer -q -M set "$VOLUMECHANNEL" "${FADEVOLUME}%"
         sleep $SPEED
     done
 fi
-
-VOLUMEALSA=$(amixer -M get $VOLUMECHANNEL | grep -m 1 -o "...%" |head -1)
-VOLUMEALSA=${VOLUMEALSA//[^[:digit:].]/}
-echo "$VOLUMEALSA"
